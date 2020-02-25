0b870fa7acd73a86c6e4e417d1f80a13f2dbf20a
nikola-mirc <nikola.mirch@gmail.com>
2020-02-25 13:44:30 +0100

    submission

---
+++ README.md
@@ -0,0 +1,10 @@
+# sugarhigh
+
+
+In the solution I came up with, I decided to convert an Array which was given as an argument to a List in order to gain ability to easily manipulate elemenets. I thought it was a good idea to use streams feature.
+
+My general idea was to sort all of the elements in their natural order (from lowest to highest) and then, using the 'foreach' loop we iterate through elements and sum them up until we hit the condition of '< threshold'. After we sum up a suitable we add them to the special 'filteredList' which contains our wanted combination of sum of lowest elements up to a certain threshold.
+
+Getting the selected elements indices was simple, since we already had a list of candies in their original order.
+
+The app is supposed be started in IDE just by running Main.class.
---
+++ src/nikola/mirc/Main.java
@@ -0,0 +1,36 @@
+package nikola.mirc;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(Arrays.toString(sugarHigh(new int[] { 33, 20, 12, 19, 29 }, 33)));
+		System.out.println(Arrays.toString(sugarHigh(new int[] { 62, 67, 100 }, 8)));
+		System.out.println(Arrays.toString(sugarHigh(new int[] { 16, 39, 67, 16, 38, 71 }, 17)));
+		System.out.println(Arrays.toString(sugarHigh(new int[] { 16, 3, 14, 17, 11 }, 99)));
+	}
+
+	public static int[] sugarHigh(int candies[], int threshold) {
+
+		int sum = 0;
+		int[] result;
+		List<Integer> candyList = Arrays.stream(candies).boxed().collect(Collectors.toList());
+		List<Integer> sortedCandyList = Arrays.stream(candies).sorted().boxed().collect(Collectors.toList());
+		List<Integer> filteredList = new ArrayList<Integer>();
+
+		for (Integer candy : sortedCandyList) {
+			if (sum + candy < threshold) {
+				sum += candy;
+				filteredList.add(candyList.indexOf(candy));
+			} else {
+				break;
+			}
+		}
+		result = filteredList.stream().sorted().mapToInt(e -> e).toArray();
+		return result;
+	}
+}
